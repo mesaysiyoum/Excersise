a8e5fc9571eb08baa4a2a334a2713064a2995c20
Mesay Siyoum <fevmes12@gmail.com>
2021-06-19 11:08:28 -0600

    Excersise in NetBeans

---
+++ MyExcersise/src/main/java/com/m/s/myexcersise/Exercise.java
@@ -0,0 +1,16 @@
+
+package com.m.s.myexcersise;
+
+/**
+ * By Mesay Siyoum
+ * @author mesay-work
+ */
+public class Exercise {
+    public static void main(String[] args) {
+        System.out.println("this is my exercise on Javai am going to save "
+                + "this java file on my computer and try to push it to the "
+                + "new repository that i created on GitHub. This is fun!" );
+    }
+           
+    
+}
