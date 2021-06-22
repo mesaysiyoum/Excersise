ba9a00209047709c5fc57c1149718f74381f0c31
mesaysiyoum <85816034+mesaysiyoum@users.noreply.github.com>
2021-06-21 19:13:36 -0600

    Fevcommit

---
+++ MyExcersise/src/main/java/com/m/s/myexcersise/Exercise2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.m.s.myexcersise;
+
+/**
+ *
+ * @author mesay-work
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println("feven is my wife and i love her!");
+        //i write this to show my wife 
+    }
+    
+}
